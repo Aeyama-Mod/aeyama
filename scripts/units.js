20527f16751ebf6900b6aea7b0370cc4f0014bbe
FredyJabe <fredypimp@gmail.com>
2023-01-17 23:46:49 -0500

    [Unit] Changes to hitSizes and insect-crawler fix

--- scripts/units.js
+++ scripts/units.js
@@ -1,4 +1,5 @@
 Events.on(ContentInitEvent, e => {
     // Makes sure that enemies can't go over walls
     Vars.content.unit("aeyama-unit-insect-swarmer").allowLegStep = false;
+    Vars.content.unit("aeyama-unit-insect-crawler").allowLegStep = false;
 });
