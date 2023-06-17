df48c2412585ae98df06952a0b22a33f3eb3a7b6
JojoFR1 <jojocraft692@gmail.com>
2023-06-17 22:34:54 +0200

    1st sector objectives

--- src/aeyama/content/AeyamaSectors.java
+++ src/aeyama/content/AeyamaSectors.java
@@ -8,11 +8,22 @@ public class AeyamaSectors {
     
     public static void load() {
         newWorld = new SectorPreset("new-world", AeyamaPlanets.aeyama, 0) {{
-            alwaysUnlocked = true;
             difficulty = 0;
+            alwaysUnlocked = true;
+
+            rules = r -> {
+                r.loadout.clear();
+                r.loadout.addAll(ItemStack.with(AeyamaItems.woodLumber, 75, AeyamaItems.stone, 50));
+                r.canGameOver = false;
+            };
         }};
         encounter = new SectorPreset("encounter", AeyamaPlanets.aeyama, 14) {{
             difficulty = 1;
+            allowLaunchSchematics = true;
+            
+            rules = r -> {
+                r.loadout.clear();
+            };
         }};
     }
 }
