35f652c0246a9a908b8d2eada261881afeb3f977
JojoFR1 <jojocraft692@gmail.com>
2023-07-21 00:59:58 +0200

    Better support for News UI on landscape mode

--- src/aeyama/ui/dialogs/NewsDialog.java
+++ src/aeyama/ui/dialogs/NewsDialog.java
@@ -22,41 +22,82 @@ public class NewsDialog extends BaseDialog {
         addCloseListener();
         buttons.button("@close", Icon.cancel, this::hide).size(256f, 64f);
 
+        Table news = getNews();
+
+        onResize(() -> {
+            cont.clear();
+            loadBody(news);
+            loadButtons();
+        });
+    
+        show();
+    }
+
+    private void loadBody(Table news) {
         cont.image(Core.atlas.find("aeyama-logo", Core.atlas.find("clear")))
             .height(mobile ? 144f : 185f).width(mobile ? 480f : 620f).pad(3f).center()
             .row();
-        cont.pane(getNews()).width(mobile ? 480f : 600f)
-            .maxWidth(mobile ? 480f : 600f).pad(4f).row();
         
-        cont.table(t -> {
-            t.defaults().size(256f, 64f).pad(3f);
-            t.button(Core.bundle.format("linkGithub"), Icon.githubSquare, () -> {
-                if (!Core.app.openURI(urlGithub)) {
-                    Vars.ui.showErrorMessage("@linkfail");
-                    Core.app.setClipboardText(urlGithub);
-                }
-            });
-            t.button(Core.bundle.format("linkDiscord"), Icon.discord, () -> {
-                if (!Core.app.openURI(urlDiscord)) {
-                    Vars.ui.showErrorMessage("@linkfail");
-                    Core.app.setClipboardText(urlDiscord);
-                }
-            }).row();
-        }).center().fillX().row();
-        cont.table(t -> {
-            t.button(Core.bundle.format("linkProject"), Icon.trello, () -> {
-                if (!Core.app.openURI(urlProject)) {
-                    Vars.ui.showErrorMessage("@linkfail");
-                    Core.app.setClipboardText(urlProject);
-                }
-            }).size(256f*2f, 64f);
-        }).center().fillX();
+        cont.pane(news).width(mobile ? 480f : 600f)
+            .maxWidth(mobile ? 480f : 600f).pad(4f);
+    }
 
-        show();
+    private void loadButtons() {
+        //Check if not on mobile Landscape mode
+        if (!(mobile && !Core.graphics.isPortrait())) {
+            cont.row();
+            
+            cont.table(t -> {
+                t.defaults().size(256f, 64f).pad(3f);
+                t.button(Core.bundle.format("linkGithub"), Icon.githubSquare, () -> {
+                    if (!Core.app.openURI(urlGithub)) {
+                        Vars.ui.showErrorMessage("@linkfail");
+                        Core.app.setClipboardText(urlGithub);
+                    }
+                });
+                t.button(Core.bundle.format("linkDiscord"), Icon.discord, () -> {
+                    if (!Core.app.openURI(urlDiscord)) {
+                        Vars.ui.showErrorMessage("@linkfail");
+                        Core.app.setClipboardText(urlDiscord);
+                    }
+                }).row();
+            }).center().fillX().row();
+            cont.table(t -> {
+                t.button(Core.bundle.format("linkProject"), Icon.trello, () -> {
+                    if (!Core.app.openURI(urlProject)) {
+                        Vars.ui.showErrorMessage("@linkfail");
+                        Core.app.setClipboardText(urlProject);
+                    }
+                }).size(256f*2f, 64f);
+            }).center().fillX();
+        } else { // If on landscape mobile
+            cont.table(t -> {
+                t.defaults().size(256f, 64f).pad(3f);
+                t.button(Core.bundle.format("linkGithub"), Icon.githubSquare, () -> {
+                    if (!Core.app.openURI(urlGithub)) {
+                        Vars.ui.showErrorMessage("@linkfail");
+                        Core.app.setClipboardText(urlGithub);
+                    }
+                }).row();
+                t.button(Core.bundle.format("linkDiscord"), Icon.discord, () -> {
+                    if (!Core.app.openURI(urlDiscord)) {
+                        Vars.ui.showErrorMessage("@linkfail");
+                        Core.app.setClipboardText(urlDiscord);
+                    }
+                }).row();
+                t.button(Core.bundle.format("linkProject"), Icon.trello, () -> {
+                    if (!Core.app.openURI(urlProject)) {
+                        Vars.ui.showErrorMessage("@linkfail");
+                        Core.app.setClipboardText(urlProject);
+                    }
+                });
+            }).center().fillX();
+        }
     }
 
     private Table getNews() {
         Table table = new Table();
+        Log.info("[green][Aeyama][lightgray] Fetching news...");
 
         Http.get(Core.bundle.format("urlNews")).error(err -> {
             table.add("[red][ERROR] FAILED TO GET NEWS.").center();
