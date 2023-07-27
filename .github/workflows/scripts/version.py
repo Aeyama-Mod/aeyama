be5d42a2b1b0c39caac2259fccb1efb2e07f3ea2
JojoFR1 <jojocraft692@gmail.com>
2023-07-27 03:24:13 +0200

    Same for main

--- .github/workflows/scripts/version.py
+++ .github/workflows/scripts/version.py
@@ -1,14 +1,33 @@
 ### Script by @jojofr.
 
-with open('mod.json', 'r+', encoding="utf8") as f:
-    lines = f.readlines()
+def modUpdate():
+    with open('mod.json', 'r+', encoding="utf8") as modFile:
+        modLines = modFile.readlines()
 
-    f.seek(0)
-    f.truncate() #Remove all data from the files
-    for num, line in enumerate(lines): 
-        if line.strip().startswith("\"version\":"):
-            versionLine = lines[num].split()
-            # Yes it's a mess. Yes it's unreadble. Yes I don't care.
-            lines[num] = "    " + versionLine[0] + " " + versionLine[1].split('dev.')[0] + 'dev.' + str(int(versionLine[1].split('dev.')[1].strip(',').strip('"')) + 1) + "\",\n"    
-        f.write(lines[num]) #Rewrite the updated value
-    f.close()
+        modFile.seek(0)
+        modFile.truncate() #Remove all data from the files
+        for num, line in enumerate(modLines): 
+            if line.strip().startswith("\"version\":"):
+                versionLine = modLines[num].split()
+                # Yes it's a mess. Yes it's unreadble. Yes I don't care.
+                modLines[num] = "    " + versionLine[0] + " " + versionLine[1].split('dev.')[0] + 'dev.' + str(int(versionLine[1].split('dev.')[1].strip(',').strip('"')) + 1) + "\",\n"    
+            modFile.write(modLines[num]) #Rewrite the updated value
+        modFile.close()
+
+def gradleUpdate():
+    with open('build.gradle', 'r+', encoding="utf8") as gradleFile:
+        gradleLines = gradleFile.readlines()
+
+        gradleFile.seek(0)
+        gradleFile.truncate() #Remove all data from the files
+        for num, line in enumerate(gradleLines): 
+            if line.strip().startswith("version"):
+                versionLine = gradleLines[num].split()
+                # Yes it's a mess. Yes it's unreadble. Yes I don't care.
+                gradleLines[num] = versionLine[0] + " " + versionLine[1].split('dev.')[0] + 'dev.' + str(int(versionLine[1].split('dev.')[1].strip("'")) + 1) + "'\n"    
+            gradleFile.write(gradleLines[num]) #Rewrite the updated value
+        gradleFile.close()
+    
+modUpdate()
+# Works but not sure if I want to use it.
+# gradleUpdate()
