a4a0b2cf8fc21c3d94919f55348e21e1935e80db
JojoFR1 <jojofr1@users.noreply.github.com>
2023-09-20 21:30:24 +0200

    Part 2 of the Part 3

--- .github/workflows/scripts/bundle.py
+++ .github/workflows/scripts/bundle.py
@@ -1,4 +1,5 @@
 import os
+import io
 
 class AutoBundles:
     PATH = "./bundles/"
@@ -8,10 +9,41 @@ class AutoBundles:
     def __init__(self):
         self.lines = self.read_lines(self.PATH + self.MAIN_FILE)
         self.old_lines = self.read_lines(self.PATH + self.OLD_FILE)
+        self.files = self.open_files()        
         
-        #* Modify/Remove/Add lines support
+        self.modify()
         
         self.write()
+        self.close_files()
+        
+    def modify(self):
+        changes = {'modified': [], 'added': [], 'removed': []}
+        for line, old_line in zip(self.lines, self.old_lines):
+            if line['type'] == 'property' and old_line['type'] == 'property':
+                if line['description'] != old_line['description']:
+                    changes['modified'].append(line['description'])
+                if line['name'] != old_line['name']:
+                    changes['modified'].append(line['name'])
+                    
+    
+    def open_files(self) -> list[io.TextIOWrapper]:
+        files = []
+        for file in os.listdir(self.PATH):
+            files.append(open(self.PATH + file, 'w', encoding='utf-8'))
+        return files
+    
+    def close_files(self):
+        for file in self.files:
+            self.files.remove(file)
+            file.close()
+            
+    def write(self):
+        for file in self.files:
+            for line in self.lines:
+                match line["type"]:
+                    case 'empty': file.write('\n')
+                    case 'comment': file.write(f'{line["content"]}\n')
+                    case 'property': file.write(f'{line["name"]}={line["description"]}\n')
         
     def read_lines(self, path):
         lines = []
@@ -21,31 +53,11 @@ class AutoBundles:
                 if not line: lines.append({'type': 'empty', 'content': '', 'line_number': line_number})
                 elif line.startswith('#'): lines.append({'type': 'comment', 'content': line, 'line_number': line_number})
                 else:
-                    key, value = line.split('=', 1)
-                    lines.append({'type': 'property', 'key': key, 'value': value, 'line_number': line_number})
+                    name, description = line.split('=', 1)
+                    lines.append({'type': 'property', 'name': name, 'description': description, 'line_number': line_number})
             file.close()
         return lines
 
-    def write(self):
-        for file in os.listdir(self.PATH):
-            if not file.startswith("bundle_") and not file.endswith(".properties") and file == self.MAIN_FILE:
-                return
-            with open(self.PATH + file, 'w', encoding='utf-8') as f:
-                for prop in self.lines:
-                    match prop["type"]:
-                        case 'empty': f.write('\n')
-                        case 'comment': f.write(f'{prop["content"]}\n')
-                        case 'property': f.write(f'{prop["key"]}={prop["value"]}\n')
-                f.close()
-    
-    def modify(self):
-        ...
-    
-    def add(self):
-        ...
-        
-    def remove(self):
-        ...
 
 if __name__ == "__main__":
     auto = AutoBundles()
