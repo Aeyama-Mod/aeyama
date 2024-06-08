ddbc3b5168e9a2366edaf2a15806279ee030d2ba
JojoFR1 <JojoFR1@users.noreply.github.com>
2024-06-08 20:42:56 +0200

    Rebirth

--- src/aeyama/Aeyama.java
+++ src/aeyama/Aeyama.java
@@ -1,45 +1,19 @@
 package aeyama;
 
-import mindustry.*;
+import arc.util.*;
 import mindustry.mod.*;
-import mindustry.type.*;
-
-import aeyama.content.*;
-import aeyama.ui.dialogs.*;
-
-import static arc.Core.*;
 
 public class Aeyama extends Mod {
 
     @Override
     public void init() {
         AeyamaVars.load();
-        SettingsMenuDialog.load();
-
-        if (settings.getBool("aeyama-showNews")) NewsDialog.load();
-        if (settings.getBool("aeyama-checkUpdate")) AeyamaUpdater.check();
-
-        // Hide the items of this mod on all the other planets.
-        for (Planet planet : Vars.content.planets()) {
-            if (planet.name != "aeyama")
-                planet.hiddenItems.addAll(AeyamaItems.aeyamaItems);
-        }
+        
+        Log.info("Aeyama Reborn!");
     }
 
     @Override
     public void loadContent() { // The load order is VERY IMPORTANT, don't change it.
-        AeyamaAttributes.load();
-        AeyamaStatusEffects.load();
-        AeyamaLoadouts.load();
-
-        AeyamaWeapons.load();
-        AeyamaUnits.load();
-        AeyamaItems.load();
-        AeyamaLiquids.load();
-        AeyamaBlocks.load();
-
-        AeyamaPlanets.load();
-        AeyamaSectors.load();
-        AeyamaTechTree.load();
+        
     }
 }
