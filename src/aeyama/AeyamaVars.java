4bf0cd3b66fab049c561ed0a32029f23216ac470
JojoFR1 <JojoFR1@users.noreply.github.com>
2024-06-08 21:07:29 +0200

    [Utils] Added a `isReborn` boolean to Vars

--- src/aeyama/AeyamaVars.java
+++ src/aeyama/AeyamaVars.java
@@ -8,9 +8,11 @@ public class AeyamaVars {
     public static String repo = "Aeyama-Mod/aeyama";
     public static boolean isDev;
     public static boolean isBeta;
+    public static boolean isReborn;
 
     public static void load() {
         isDev = mod.meta.version.contains("dev");
         isBeta = mod.meta.version.contains("beta") || mod.meta.version.contains("pre");
+        isReborn = mod.meta.version.substring(0, mod.meta.version.indexOf("-")).contains("r");
     }
 }
